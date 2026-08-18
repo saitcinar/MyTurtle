215b61e5d8bb86990baaab0c9b5d1d606993a40b
Sait Çınar <saitcinar@gmail.com>
2026-08-18 15:10:23 +0300

    my first drawing

---
+++ main.py
@@ -0,0 +1,13 @@
+import turtle
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("green")
+drawing_board.title("Python Turtle")
+turtle_instance = turtle.Turtle()
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle_instance.left(90)
+turtle_instance.forward(100)
+turtle.done()
